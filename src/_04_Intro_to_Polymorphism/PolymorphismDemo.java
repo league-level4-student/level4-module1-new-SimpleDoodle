3368712f1f8fce034d130e00ff662a426b1a3dc5
jereycui2008 <jeremycuid@gmail.com>
2023-09-14 18:30:06 -0700

    Polymorph

--- src/_04_Intro_to_Polymorphism/PolymorphismDemo.java
+++ src/_04_Intro_to_Polymorphism/PolymorphismDemo.java
@@ -12,11 +12,17 @@ abstract class Animal {
     public void speak() {
         System.out.println("I am an animal");
     }
+    public void eat() {
+    	System.out.println("I want food");
+    }
 }
 
 class Dog extends Animal {
     public void speak() {
         System.out.println("Woof!");
+    }
+    public void eat(){
+    	System.out.println("Meat!");
     }
 }
 
@@ -45,8 +51,9 @@ public class PolymorphismDemo {
          * Bird or Fish on the right side of the equals sign while still being
          * declared as an Animal on the left side.
          */
-
-        Animal animal = new Animal();
+    	
+    	
+        Animal animal = new Dog();
 
         /*
          * 2. Call the speak method and run the program.
@@ -54,7 +61,11 @@ public class PolymorphismDemo {
          * Even though the variable was declared as an Animal, at run time it
          * will behave like whatever Animal subclass it was instantiated as.
          */
+        
+        animal.speak();
+        animal.eat();
 
+        
         /*
          * 3. Comment out the speak method of the Animal subclass you chose and
          * run the program again.
@@ -64,6 +75,8 @@ public class PolymorphismDemo {
          * called. In this case it was Animal class's version of speak.
          */
 
+        
+        
         /*
          * 4. Add another method called eat to the Animal subclass you chose and
          * have it print a description of the animal eating their favorite food
@@ -83,7 +96,9 @@ public class PolymorphismDemo {
          * 6. Try casting animal into the Animal subclass you chose and calling
          * the eat method.
          */
-
+        
+        
+        
         /*
          * Polymorphism is not just limited to variables, it can also be used in
          * arrays, ArrayLists or any other data structures you are familiar
